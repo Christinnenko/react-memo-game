2de859cfa5074bdda7967a9b27a2b1d8f1b2e8c9
Christinnenko <Christinnenko@yandex.ru>
2024-04-19 04:48:06 +0300

    Updates

--- src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Button.module.css";
-
-export function Button({ disabled, children, onClick }) {
-  return (
-    <button disabled={disabled} onClick={onClick} className={styles.button}>
-      {children}
-    </button>
-  );
-}
